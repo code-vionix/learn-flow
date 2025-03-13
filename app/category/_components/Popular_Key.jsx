7376a065808c0c67b52e12d2469e89a2de4f7bf1
Md Mim Shifat <shifat58201@gmail.com>
2025-03-14 04:57:47 +0600

    feat: category page

---
+++ app/category/_components/Popular_Key.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function PopularKeyword() {
+  const keywords = [
+    "HTML 5",
+    "Web Development",
+    "Responsive Development",
+    "Developments",
+    "Programming",
+    "Website",
+    "Technology",
+    "WordPress",
+  ];
+
+  const [selectedKeyword, setSelectedKeyword] = useState(null);
+
+  return (
+    <div className="pt-10 pb-20 flex items-center gap-6 flex-wrap">
+      <h3 className="text-[18px] font-medium text-gray-600">
+        Popular keywords:
+      </h3>
+      <div className="flex flex-wrap gap-2 cursor-pointer">
+        {keywords.map((keyword, index) => (
+          <span
+            key={index}
+            onClick={() => setSelectedKeyword(keyword)}
+            className={`${
+              selectedKeyword === keyword
+                ? "bg-primary-500 text-white"
+                : "bg-gray-100 text-gray-700"
+            } py-1.5 px-3 gap-2.5 rounded-md text-sm font-medium`}
+          >
+            {keyword}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
