bbfa5fbe6f8239896072884a02981ca89279b6bb
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-03-14 17:18:23 +0600

    bug fixd

--- app/student/_components/StudentLayout.jsx
+++ app/student/_components/StudentLayout.jsx
@@ -6,7 +6,7 @@ const StudentLayout = () => {
   return (
     <div className="w-full h-40 bg-[#FFEEE8] relative">
       <div className="container mx-auto absolute top-3/4  bg-white left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-        <div className="border-b border-">
+        <div className="border-b border-primary-200">
           <div className="py-6 px-4 md:px-6 flex flex-col md:flex-row items-start md:items-center justify-between">
             <div className="flex items-center gap-4">
               <Image
@@ -35,7 +35,7 @@ const StudentLayout = () => {
           </div>
           <nav className="flex w-full justify-between overflow-x-auto border-t border-primary-200">
             <Link
-              href="/student/dashboard"
+              href="/student"
               className="px-6 py-4 text-gray-600 hover:text-gray-900 border-b-2 border-transparent hover:border-primary-500"
             >
               Dashboard
