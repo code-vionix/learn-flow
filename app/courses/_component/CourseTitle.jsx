dfb2e49f0e698100a2d9f8e5cf1837d1953209fd
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-03-15 15:34:51 +0600

    bug fixd:single page position bug fixd

--- app/courses/_component/CourseTitle.jsx
+++ app/courses/_component/CourseTitle.jsx
@@ -1,7 +1,9 @@
 export default function CourseTitle({ title, subtitle }) {
   return (
-    <div className="space-y-4">
-      <h1 className="text-3xl font-semibold text-gray-900">{title}</h1>
+    <div className="py-2">
+      <h3 className="text-3xl font-semibold text-gray-900 max-w-[700px]">
+        {title}
+      </h3>
       <p className="text-xl text-gray-700">{subtitle}</p>
     </div>
   );
