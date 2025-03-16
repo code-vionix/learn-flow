383368c05c68f91e93127e759c96fca9f55172e9
Md Mim Shifat <shifat58201@gmail.com>
2025-03-16 23:07:39 +0600

    fix: color

--- app/(main)/components/shared/social_icon.jsx
+++ app/(main)/components/shared/social_icon.jsx
@@ -13,19 +13,19 @@ import whatsapp from "@/public/whatsapp.png";
 export default function SocialIcon() {
     return (
         <div className="flex gap-4 mt-4 self-end">
-            <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+            <a href="#" className="p-3 bg-gray-100 hover:bg-primay-500">
                 <Facebook className="h-4 w-4 text-black" />
             </a>
-            <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+            <a href="#" className="p-3 bg-gray-100 hover:bg-primary-500">
                 <Twitter className="h-4 w-4 text-black" />
             </a>
-            <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+            <a href="#" className="p-3 bg-gray-100 hover:bg-primary-500">
                 <Instagram className="h-4 w-4 text-black" />
             </a>
-            <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+            <a href="#" className="p-3 bg-gray-100 hover:bg-primary-500">
                 <Youtube className="h-4 w-4 text-black" />
             </a>
-            <a href="https://wa.me/" className="p-3 bg-gray-100 hover:bg-orange-500">
+            <a href="https://wa.me/" className="p-3 bg-gray-100 hover:bg-primary-500">
                 <Image
                     src={whatsapp}
                     alt="WhatsApp"
