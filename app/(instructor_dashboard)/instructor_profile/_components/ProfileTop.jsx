16073a8c4feca46bb3b883bae6824bca08e26cb9
Md Mim Shifat <shifat58201@gmail.com>
2025-03-16 21:55:08 +0600

    feat: instructor profile

---
+++ app/(instructor_dashboard)/instructor_profile/_components/ProfileTop.jsx
@@ -0,0 +1,88 @@
+import {
+  BookOpen,
+  Crown,
+  Facebook,
+  Globe,
+  Instagram,
+  Star,
+  Twitter,
+  Users,
+  Youtube,
+} from "lucide-react";
+import Image from "next/image";
+import { FaWhatsapp } from "react-icons/fa";
+import playCircle from "@/public/PlayCircle.png";
+
+export default function ProfileTop() {
+  return (
+    <div className="h-40 bg-[#FFEEE8] relative">
+      <div className="container mx-auto px-8 absolute top-3/4 bg-white left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="border-b border-primary-200">
+          <div className="py-6 px-4 md:px-6 flex flex-col md:flex-row items-start md:items-center justify-between">
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
+              <Image
+                src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Profile picture"
+                width={200}
+                height={200}
+                className="rounded-full w-36 h-36 object-cover"
+              />
+
+              <div className="flex-1">
+                <div className="flex items-center gap-2 mb-3">
+                  <h2 className="text-2xl font-semibold">Vako Shvili</h2>
+                  <span className="flex items-center gap-1 bg-orange-100 text-orange-600 text-xs font-bold px-2 py-1 rounded-md">
+                    <Crown className="w-4 h-4" /> Top Rated
+                  </span>
+                </div>
+                <p className="text-gray-500 text-sm mb-5">
+                  Web Designer & Best-Selling Instructor
+                </p>
+                <div className="flex items-center gap-4 mt-2 text-gray-600 text-sm">
+                  <div className="flex items-center gap-1">
+                    <Star className="text-yellow-500" size={16} />{" "}
+                    <span className="font-semibold">4.8</span> (134,633 reviews)
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Users size={16} />{" "}
+                    <span className="font-semibold">430,117</span> students
+                  </div>
+                  <div className="flex items-center gap-1">
+                  <Image src={playCircle} alt="Play Circle" width={20} height={20} />
+
+                    <span className="font-semibold">7</span> courses
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col md:items-end gap-3 mt-3 md:mt-0">
+              <a
+                href="https://www.vakoshvili.com"
+                className="text-blue-500 hover:underline flex items-center gap-2"
+              >
+                <Globe size={16} className="text-end" /> www.vakoshvili.com
+              </a>
+              <div className="flex gap-4 mt-4 self-end">
+                <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+                  <Facebook className="h-4 w-4 text-black" />
+                </a>
+                <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+                  <Twitter className="h-4 w-4 text-black" />
+                </a>
+                <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+                  <Instagram className="h-4 w-4 text-black" />
+                </a>
+                <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+                  <Youtube className="h-4 w-4 text-black" />
+                </a>
+                <a href="#" className="p-3 bg-gray-100 hover:bg-orange-500">
+                  <FaWhatsapp className="h-4 w-4 text-black" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
