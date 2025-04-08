dd48c71e1c5750498a6caee6a7e78c6183de8120
MotiurRahman <rmdmotiur144@gmail.com>
2025-04-08 21:15:05 +0600

    faqs page create

---
+++ app/(main)/FAQs/components/QuestionSet.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function QuestionSet({selectedTopic,setSelectedTopic,topics,role}) {
+  return (
+    <div className="lg:col-span-3">
+      <div className="bg-white rounded-lg shadow">
+      {topics[role].map((topic) => (
+                <button
+                  key={topic}
+                  onClick={() => setSelectedTopic(topic)}
+                  className={`w-full text-left px-4 py-3 border-b last:border-b-0 hover:bg-gray-50 transition-colors ${
+                    selectedTopic === topic
+                      ? 'bg-primary-500 text-white hover:bg-primary-600'
+                      : 'text-gray-700'
+                  }`}
+                >
+                  {topic}
+                </button>
+              ))}
+      </div>
+    </div>
+  );
+}
+
+export default QuestionSet;
