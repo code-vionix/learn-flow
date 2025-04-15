a97415186d9989d846a471b4a2f533e608e8e3ea
MotiurRahman <rmdmotiur144@gmail.com>
2025-04-15 23:28:28 +0600

    bug fix

--- app/(main)/about/page.js
+++ app/(main)/about/page.js
@@ -14,9 +14,9 @@ function AboutPage() {
       <div className="px-2 py-2 sm:px-8 md:px-32 xl:px-40 2xl:px-72  " >
         <AboutUs />
         <div className="mt-0">
-        {/* <TrustedCompanies smHeading={"We just keep going with "} /> */}
+        <TrustedCompanies smHeading={"We just keep going with "} />
         </div>
-        {/* <InstructorDisplayInfo color={"bg-white"} /> */}
+        <InstructorDisplayInfo color={"bg-white"} />
       </div>
       <div className="bg-primary-100">
         <OurMission />
