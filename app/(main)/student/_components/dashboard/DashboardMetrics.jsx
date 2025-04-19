c98eb6f2abfd16c586dae66193b9963d25b63f1b
shifat025 <shifat58201@gmail.com>
2025-04-19 17:38:40 +0600

    fix: student enrollment courses

--- app/(main)/student/_components/dashboard/DashboardMetrics.jsx
+++ app/(main)/student/_components/dashboard/DashboardMetrics.jsx
@@ -1,40 +1,40 @@
 import { Card } from "@/components/ui/card";
-import { cn, nameToColor } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import { Flag, Play, Trophy, Users } from "lucide-react";
 import Link from "next/link";
 
-const metrics = [
-  {
-    label: "Enrolled Courses",
-    value: "957",
-    icon: Play,
-    className: "bg-orange-100",
-    iconClassName: "text-orange-500",
-  },
-  {
-    label: "Active Courses",
-    value: "6",
-    icon: Flag,
-    className: "bg-blue-100",
-    iconClassName: "text-blue-500",
-  },
-  {
-    label: "Completed Courses",
-    value: "951",
-    icon: Trophy,
-    className: "bg-green-100",
-    iconClassName: "text-green-500",
-  },
-  {
-    label: "Course Instructors",
-    value: "241",
-    icon: Users,
-    className: "bg-orange-100",
-    iconClassName: "text-orange-500",
-  },
-];
+export default function DashboardMetrics({ totalEnroll }) {
+  const metrics = [
+    {
+      label: "Enrolled Courses",
+      value: totalEnroll,
+      icon: Play,
+      className: "bg-orange-100",
+      iconClassName: "text-orange-500",
+    },
+    {
+      label: "Active Courses",
+      value: "6",
+      icon: Flag,
+      className: "bg-blue-100",
+      iconClassName: "text-blue-500",
+    },
+    {
+      label: "Completed Courses",
+      value: "951",
+      icon: Trophy,
+      className: "bg-green-100",
+      iconClassName: "text-green-500",
+    },
+    {
+      label: "Course Instructors",
+      value: "241",
+      icon: Users,
+      className: "bg-orange-100",
+      iconClassName: "text-orange-500",
+    },
+  ];
 
-export default function DashboardMetrics({totalEnroll}) {
   return (
     <>
       {metrics.map((mat, index) => (
@@ -45,12 +45,11 @@ export default function DashboardMetrics({totalEnroll}) {
               mat.className
             )}
           >
-            <div className="p-3 gap-4 bg-white">
+            <div className="p-3 bg-white rounded-full">
               <mat.icon className={`w-6 h-6 ${mat.iconClassName}`} />
             </div>
             <div>
-              <p className="text-xl font-bold text-gray-600">{totalEnroll}</p>
-
+              <p className="text-xl font-bold text-gray-600">{mat.value}</p>
               <h3 className="font-semibold text-sm text-gray-500">
                 {mat.label}
               </h3>
