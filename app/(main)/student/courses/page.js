72598e0cf3789554a41bf7710a856c66e87a1470
shifat025 <shifat58201@gmail.com>
2025-04-19 17:38:40 +0600

    fix: student enrollment courses

--- app/(main)/student/courses/page.js
+++ app/(main)/student/courses/page.js
@@ -1,36 +1,14 @@
-// import { fetchCourses } from "@/lib/fetchData";
-// import Heading from "../_components/courses/Heading";
-// import CourseCard from "../_components/shared/CourseCard";
-
-// export default async function CoursesPage() {
-//   const courses = await fetchCourses();
-//   return (
-//     <div className="container mx-auto ">
-//       <Heading courses={courses} />
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-//         {courses.map((course) => (
-//           <div key={course.id} className="w-full h-92">
-//             <CourseCard course={course} className="h-full" />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
-import { useGetAllCourseQuery } from "@/store/api/courseApi";
 import { useGetAllEnrollCoursesQuery } from "@/store/api/enrolledCourse";
 import Heading from "../_components/courses/Heading";
 import CourseCard from "../_components/shared/CourseCard";
 
 export default function CoursesPage() {
   const {
-    data: enrollcourses,
+    data: courses,
     isLoading,
     isError,
   } = useGetAllEnrollCoursesQuery("67e3de89529e63530afa1c88");
-  const { data: allCourses } = useGetAllCourseQuery();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -40,17 +18,13 @@ export default function CoursesPage() {
     return <div>Error fetching courses</div>;
   }
 
-  const courses = (allCourses || []).filter((course) =>
-    (enrollcourses || []).some((enrolled) => enrolled.courseId === course.id)
-  );
-
   return (
     <div className="container mx-auto">
       <Heading courses={courses} />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {courses.map((course) => (
           <div key={course.id} className="w-full h-92">
-            <CourseCard course={course} className="h-full" />
+            <CourseCard course={course.course} className="h-full" />
           </div>
         ))}
       </div>
--- app/(main)/student/page.js
+++ app/(main)/student/page.js
@@ -1,27 +1,18 @@
 "use client";
 
-import { useGetAllCourseQuery } from "@/store/api/courseApi";
 import { useGetAllEnrollCoursesQuery } from "@/store/api/enrolledCourse";
 import { CourseCarousel } from "./_components/dashboard/CourseCarousel";
 import DashboardMetrics from "./_components/dashboard/DashboardMetrics";
 
 export default function HomePage() {
   const {
-    data: enrollcourses,
+    data: courses,
     isLoading,
     isError,
   } = useGetAllEnrollCoursesQuery("67e3de89529e63530afa1c88");
-  const { data: allCourses } = useGetAllCourseQuery();
-
-  const courses = (allCourses || []).filter((course) =>
-    (enrollcourses || []).some((enrolled) => enrolled.courseId === course.id)
-  );
-  console.log(courses);
-
-  // console.log(courses);
 
   if (isLoading) return <p>Loading...</p>;
-  if ( isError) return <p>Something went wrong!</p>;
+  if (isError) return <p>Something went wrong!</p>;
 
   const total_enrolled = courses.length;
 
