cb3a541b4e9c6f450b6f8f707922dcc6bd4a9899
shifat025 <shifat58201@gmail.com>
2025-05-03 13:28:24 +0600

    feat: studen dashboard

--- app/(main)/student/purchase-history/_components/PurchaseCard.jsx
+++ app/(main)/student/purchase-history/_components/PurchaseCard.jsx
@@ -4,27 +4,33 @@ import Image from "next/image";
 import Link from "next/link";
 import PurchaseOrderSummaryCard from "./PurchaseOrderSummaryCard";
 import PurchaseItem from "./PurchaseItem";
+import { formatEnrollmentDate } from "@/lib/formatTime";
 
 const PurchaseCard = ({ purchase }) => {
-    const { date, courses, amount, paymentMethod, cardDetails, courseDetails } = purchase;
+    const { enrollmentDate, course:courses, payment } = purchase;
+
+    const formatedDate = formatEnrollmentDate(enrollmentDate)
+    
+    const payments = payment?.[0];
 
     return (
         <div className="">
             <PurchesAccordionItem className="border " value={`course-${purchase?.id}`}>
                 <PurchesAccordionTrigger>
                     <div className="text-start ">
-                        <h3 className="text-[18px]">{date}</h3>
+                        <h3 className="text-[18px]">{formatedDate}</h3>
                         <div className="md:flex hidden text-[14px] items-center gap-4 mt-2">
                             <div className="flex items-center gap-1 text-gray-700">
-                                <PlayCircle className="text-secondary-500 w-[15px] h-[15px]" /> {courses} Courses
+                                <PlayCircle className="text-secondary-500 w-[15px] h-[15px]" />  {courses?.length || 0} {courses?.length === 1 ? 'Course' : 'Courses'}
+                                {/* <PlayCircle className="text-secondary-500 w-[15px] h-[15px]" /> {courses} Courses */}
                             </div>
 
                             <div className="flex items-center gap-1 text-gray-700">
-                                <DollarSign className="text-primary-500 w-[15px] h-[15px]" /> {amount} USD
+                                <DollarSign className="text-primary-500 w-[15px] h-[15px]" /> {payments?.amount} USD
                             </div>
 
                             <div className="flex items-center gap-1 text-gray-700">
-                                <CreditCard className="text-success-500 w-[15px] h-[15px]" /> {paymentMethod}
+                                <CreditCard className="text-success-500 w-[15px] h-[15px]" /> {payments?.method}
                             </div>
                         </div>
                     </div>
@@ -34,8 +40,8 @@ const PurchaseCard = ({ purchase }) => {
                         {/* Left side - Course listings */}
                         <div className="flex-1 border-r pr-0">
                             <div className="space-y-">
-                                {courseDetails.map((course) => (<PurchaseItem
-                                    key={course?.id}
+                                {courses.map((course) => (<PurchaseItem
+                                    key={course.id}
                                     course={course}
 
                                 />))}
@@ -43,11 +49,11 @@ const PurchaseCard = ({ purchase }) => {
                         </div>
 
                         <PurchaseOrderSummaryCard
-                            courseDetails={courseDetails}
-                            amount={amount}
-                            cardDetails={cardDetails}
-                            date={date}
-                            paymentMethod
+                            courseDetails={courses}
+                            amount={payments?.amount}
+                            // cardDetails={cardDetails}
+                            date={formatedDate}
+                            paymentMethod = {payments?.method}
                         />
                     </div>
                 </PurchesAccordionContent>
