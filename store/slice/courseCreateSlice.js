e7a89b288ce3cca68bf1c3d52417c5bd440e3434
Tanvir <tanvir.info07@gmail.com>
2025-05-05 02:00:26 +0600

    courseBasicForm Comlete only api call left

--- store/slice/courseCreateSlice.js
+++ store/slice/courseCreateSlice.js
@@ -1,28 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { set } from "date-fns";
+
+// Async thunk to fetch categories
+export const fetchCategories = createAsyncThunk(
+  "course/fetchCategories",
+  async () => {
+    const res = await fetch(`http://localhost:3001/api/v1/category`);
+    const data = await res.json();
+    return data;
+  }
+);
 
 const initialState = {
   courseBasicData: {},
   courseAdvancedData: {},
   activeTab: "basic",
+  category: [],
+  subCategory: [],
 };
 
 const courseCreateSlice = createSlice({
   name: "course",
   initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategories.fulfilled, (state, action) => {
+      state.category = action.payload;
+    });
+  },
+
   reducers: {
-    setCourse: (state, action) => {
+    setBasicCourse: (state, action) => {
       state.courseBasicData = action.payload;
     },
     setCourseAdvancedData: (state, action) => {
       state.courseAdvancedData = action.payload;
     },
     setActiveTab: (state, action) => {
-      console.log(action.payload);
       state.activeTab = action.payload;
+    },
+
+    // manage useState in basicCourseForm
+    setSubCategory: (state, action) => {
+      state.subCategory = action.payload;
     },
   },
 });
 
-export const { setCourse, setActiveTab, setCourseAdvancedData } =
-  courseCreateSlice.actions;
+export const {
+  setBasicCourse,
+  setActiveTab,
+  setCourseAdvancedData,
+  setSubCategory,
+} = courseCreateSlice.actions;
 export default courseCreateSlice.reducer;
