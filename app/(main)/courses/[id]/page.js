d13b16345588954c77cc63344bde541d74de74d6
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-05-09 12:29:04 +0600

    single course page done

--- app/(main)/courses/[id]/page.js
+++ app/(main)/courses/[id]/page.js
@@ -1,13 +1,24 @@
-import { getCourseById } from "@/lib/fetchData";
 
+import { getCourseById } from "@/utils/getCourseById";
 import Breadcrumb from "./_component/Breadcrumb";
 
 import CourseTitle from "./_component/CourseTitle";
+import CourseInfo from "./_component/CourseInfo";
+import CoursePreview from "./_component/CoursePreview";
+import CourseTabs from "./_component/CourseTabs";
+import CourseDescription from "./_component/CourseDescription";
+import CourseLearning from "./_component/CourseLearning";
+import CourseDetails from "./_component/CourseDetails";
+import Curriculum from "./_component/Curriculum";
+import CourseInstructor from "./_component/CourseInstructor";
+import CourseRating from "./_component/CourseRating";
+import { StudentFeedback } from "./_component/StudentFeedback";
+import CourseSidebar from "./_component/CourseSidebar";
+import RelatedCourses from "./_component/RelatedCourses";
 
 export default async function Home({ params }) {
   const { id } = params;
   const course = await getCourseById(id);
-
   return (
     <div className="h-full w-full">
       <div className="w-full min-h-screen relative">
@@ -15,31 +26,31 @@ export default async function Home({ params }) {
 
         <div className="flex-1 px-10 py-2 flex gap-6 w-[1320px] mx-auto  bg-transparent -mt-[330px] ">
           <div className="flex flex-col  gap-4 w-full">
-            <Breadcrumb />
+            <Breadcrumb category={course.category} subCategory={course.subCategory}/>
             <CourseTitle title={course?.title} subtitle={course?.subtitle} />
-            {/* <CourseInfo course={course} /> */}
-            {/* <CoursePreview course={course} /> */}
-            {/* <CourseTabs /> */}
-            {/* <CourseDescription /> */}
-            {/* <CourseLearning /> */}
-            {/* <CourseDetails /> */}
+            <CourseInfo course={course} />
+            <CoursePreview course={course} />
+            <CourseTabs course={course} />
+            <CourseDescription description={course?.description} />
+            <CourseLearning learning={course?.learnings} />
+            <CourseDetails tergetAudience={course?.targetAudiences} requirements={course?.PreRequirement} />
 
             {/* Ensure the ID matches the tab name */}
-            <section id="curriculum">{/* <Curriculum /> */}</section>
+            <section id="curriculum"><Curriculum /></section>
 
             <section id="instructor">
-              {/* <CourseInstructor instructors={course?.instructors} /> */}
+              <CourseInstructor instructor={course?.instructor} />
             </section>
 
             <section id="review">
-              {/* <CourseRating /> */}
-              {/* <StudentFeedback reviews={course?.user_reviews} /> */}
+              <CourseRating reviews={course.reviews}/>
+              <StudentFeedback reviews={course.reviews} />
             </section>
           </div>
-          {/* <CourseSidebar course={course} /> */}
+          <CourseSidebar course={course} />
         </div>
       </div>
-      <div className="w-full">{/* <RelatedCourses course={course} /> */}</div>
+      <div className="w-full"><RelatedCourses course={course} /></div>
     </div>
   );
 }
