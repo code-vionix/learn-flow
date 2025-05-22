de815c06d8278290f4f7b4bf6ed3618a01d05409
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-05-22 19:19:02 +0600

    token refrash token logic aplay

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -2,13 +2,13 @@
 
 import { AuthProvider } from "@/components/AuthProvider";
 import { CourseProvider } from "@/povider/CourseProvider";
+import { store } from "@/store/store";
 import { Suspense } from "react";
+import { Provider } from "react-redux";
 import "../globals.css";
 import Footer from "./components/shared/Footer/Footer";
 import Navbars from "./components/shared/Navbars";
 import Navigation from "./components/shared/Navigation";
-import { Provider } from "react-redux";
-import { store } from "@/store/store";
 
 export default function MainLayout({ children }) {
   return (
