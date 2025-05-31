67e0b49133603fe8fac3e0db319015603d98049c
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-05-31 14:16:05 +0600

    module show bug fixd player page

--- app/(main)/player/[courseId]/_components/WatchingCourseContent.jsx
+++ app/(main)/player/[courseId]/_components/WatchingCourseContent.jsx
@@ -26,13 +26,13 @@ const WatchingCourseContent = ({ sections }) => {
 
   return (
     <div className="md:mt-0 mt-4">
-      <div className="flex items-center justify-between w-full">
+      {/* <div className="flex items-center justify-between w-full">
         <h1 className="md:text-[24px] font-semibold">Course Contents</h1>
         <h3 className="md:text-[16px] font-semibold text-success-500">
           15% Completed
         </h3>
       </div>
-      <CourseProgress />
+      <CourseProgress /> */}
 
       <div className="mt-4 border transparent-scroll h-[490px] overflow-y-auto ">
         <Accordion2
