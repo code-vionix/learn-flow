ce30f6d45840d25d53c2329f72cd45d80182e375
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-06-01 09:34:11 +0600

    single page bug fixd

--- app/(main)/courses/[id]/page.js
+++ app/(main)/courses/[id]/page.js
@@ -13,19 +13,19 @@ import CourseSidebar from "./_component/CourseSidebar";
 import CourseTabs from "./_component/CourseTabs";
 import CourseTitle from "./_component/CourseTitle";
 
-import { auth } from "@/auth";
-import { notFound, redirect } from "next/navigation";
 import Curriculum from "./_component/Curriculum/Curriculum";
 import RelatedCourses from "./_component/RelatedCourses";
 import { StudentFeedback } from "./_component/StudentFeedback";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default async function CourseDetailsPage({ params }) {
+
+
+export default async function Home({ params }) {
+  const { id } = params;
   const session = await auth();
   const userId = session?.user?.id;
-
-  const { id } = params;
-
-  let course;
+    let course;
   try {
     course = await getCourseById(id);
   } catch (error) {
@@ -35,11 +35,11 @@ export default async function CourseDetailsPage({ params }) {
   if (!course) {
     return notFound();
   }
-
   const instructor = await getCourseDataByCourseId("instructor", id);
+  console.log(JSON.stringify(instructor, null, 2));
   const reviews = await getCourseDataByCourseId("reviews", id);
 
-  const isEnrolled = course?.enrollments?.some(
+    const isEnrolled = course?.enrollments?.some(
     (enrollment) => enrollment.userId === userId
   );
 
@@ -50,33 +50,34 @@ export default async function CourseDetailsPage({ params }) {
   return (
     <div className="h-full w-full">
       <div className="w-full min-h-screen relative">
-        <div className="flex flex-col absolute top-0 h-[370px] w-full bg-gray-100"></div>
+        <div className="flex flex-col  absulate top-0 h-[370px] w-full bg-gray-100"></div>
 
-        <div className="flex-1 px-10 py-2 flex gap-6 w-[1320px] mx-auto bg-transparent -mt-[330px]">
-          <div className="flex flex-col gap-4 w-full">
+        <div className="flex-1 px-10 py-2 flex gap-6 w-[1320px] mx-auto  bg-transparent -mt-[330px] ">
+          <div className="flex flex-col  gap-4 w-full">
             <Breadcrumb
               categoryName={course?.categoryName}
               subCategoryName={course?.subCategoryName}
             />
             <CourseTitle title={course?.title} subtitle={course?.subtitle} />
-            <CourseInfo course={course} instructor={instructor?.data} />
+            <CourseInfo course={course} instructor={instructor.data} />
             <CoursePreview course={course} />
             <CourseTabs course={course} />
             <CourseDescription description={course?.description} />
             <CourseLearning id={id} />
             <CourseDetails id={id} />
 
+            {/* Ensure the ID matches the tab name */}
             <section id="curriculum">
               <Curriculum id={id} />
             </section>
 
             <section id="instructor">
-              <CourseInstructor instructor={instructor?.data} />
+              <CourseInstructor instructor={instructor.data} />
             </section>
 
             <section id="review">
-              <CourseRating reviews={reviews} />
-              <StudentFeedback reviews={reviews} />
+               <CourseRating reviews={reviews} />
+               <StudentFeedback reviews={reviews} />
             </section>
           </div>
           <CourseSidebar course={course} />
